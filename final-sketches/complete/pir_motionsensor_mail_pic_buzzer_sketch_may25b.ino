400466184b60a218406917eab6be61e45dc340ee
AW-2021 <77668986+AW-2021@users.noreply.github.com>
2022-05-30 17:58:14 +0500

    Update pir_motionsensor_mail_pic_buzzer_sketch_may25b.ino

--- final-sketches/complete/pir_motionsensor_mail_pic_buzzer_sketch_may25b.ino
+++ final-sketches/complete/pir_motionsensor_mail_pic_buzzer_sketch_may25b.ino
@@ -46,26 +46,7 @@ int notes[] = {   //HERE+
     NOTE_C5, NOTE_A4, NOTE_B4, 0,
 
     NOTE_A4, NOTE_A4,
-    //Repeat of first part
-    NOTE_A4, NOTE_B4, NOTE_C5, NOTE_C5, 0,
-    NOTE_C5, NOTE_D5, NOTE_B4, NOTE_B4, 0,
-    NOTE_A4, NOTE_G4, NOTE_A4, 0,
-
-    NOTE_E4, NOTE_G4, NOTE_A4, NOTE_A4, 0,
-    NOTE_A4, NOTE_B4, NOTE_C5, NOTE_C5, 0,
-    NOTE_C5, NOTE_D5, NOTE_B4, NOTE_B4, 0,
-    NOTE_A4, NOTE_G4, NOTE_A4, 0,
-
-    NOTE_E4, NOTE_G4, NOTE_A4, NOTE_A4, 0,
-    NOTE_A4, NOTE_C5, NOTE_D5, NOTE_D5, 0,
-    NOTE_D5, NOTE_E5, NOTE_F5, NOTE_F5, 0,
-    NOTE_E5, NOTE_D5, NOTE_E5, NOTE_A4, 0,
-
-    NOTE_A4, NOTE_B4, NOTE_C5, NOTE_C5, 0,
-    NOTE_D5, NOTE_E5, NOTE_A4, 0,
-    NOTE_A4, NOTE_C5, NOTE_B4, NOTE_B4, 0,
-    NOTE_C5, NOTE_A4, NOTE_B4, 0,
-    //End of Repeat
+    
 
     NOTE_E5, 0, 0, NOTE_F5, 0, 0,
     NOTE_E5, NOTE_E5, 0, NOTE_G5, 0, NOTE_E5, NOTE_D5, 0, 0,
@@ -101,26 +82,6 @@ int durations[] = {     //HERE+
     125, 125, 375, 375,
 
     250, 125,
-    //Rpeat of First Part
-    125, 125, 250, 125, 125,
-    125, 125, 250, 125, 125,
-    125, 125, 375, 125,
-
-    125, 125, 250, 125, 125,
-    125, 125, 250, 125, 125,
-    125, 125, 250, 125, 125,
-    125, 125, 375, 125,
-
-    125, 125, 250, 125, 125,
-    125, 125, 250, 125, 125,
-    125, 125, 250, 125, 125,
-    125, 125, 125, 250, 125,
-
-    125, 125, 250, 125, 125,
-    250, 125, 250, 125,
-    125, 125, 250, 125, 125,
-    125, 125, 375, 375,
-    //End of Repeat
 
     250, 125, 375, 250, 125, 375,
     125, 125, 125, 125, 125, 125, 125, 125, 375,
